651a5aa1785c42f506a0c113e9de58c51443ed43
LizaSamodumkina <43859393+LizaSamodumkina@users.noreply.github.com>
2018-12-23 15:01:01 +0300

    Add files via upload

--- src/by/etc/samodumkina/dao/factory/DAOFactory.java
+++ src/by/etc/samodumkina/dao/factory/DAOFactory.java
@@ -1,12 +1,15 @@
 package by.etc.samodumkina.dao.factory;
 
+import by.etc.samodumkina.dao.BookDAO;
 import by.etc.samodumkina.dao.UserDAO;
+import by.etc.samodumkina.dao.impl.SQLBookDAO;
 import by.etc.samodumkina.dao.impl.SQLUserDAO;
 
 public class DAOFactory {
 	private final static DAOFactory instance = new DAOFactory();
 	
 	private final UserDAO sqlUserImpl = new SQLUserDAO();
+	private final BookDAO sqlBookImpl = new SQLBookDAO();
 	
 	private DAOFactory() {}
 	
@@ -14,7 +17,11 @@ public class DAOFactory {
 		return instance;
 	}
 	
-	public UserDAO getUserDAO() {
+	public UserDAO takeUserDAO() {
 		return sqlUserImpl;
+	}
+	
+	public BookDAO takeBookDAO() {
+		return sqlBookImpl;
 	}
 }
